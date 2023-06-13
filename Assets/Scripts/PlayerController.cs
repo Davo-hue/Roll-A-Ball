5900450cc85a6e98515cb449f6b00064a2561809
Davo-hue <136394432+Davo-hue@users.noreply.github.com>
2023-06-13 11:42:05 +1000

    Added Pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,20 @@ public class PlayerController : MonoBehaviour
    
     public float speed = 5.0f;
     private Rigidbody rb;
+    private int pickupCount; 
     
 
-    // Start is called before the first frame update
+   
     void Start()            
     {
         rb = GetComponent<Rigidbody>();
+        // Get the number of pickups in our scene
+        pickupCount = GameObject.FindGameObjectsWithTag("Pick Up").Length;
+        // run check pickups function
+        CheckPickups();
     }
 
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -25,5 +30,29 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         rb.AddForce(movement);
         
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Pick Up")
+        {
+            Destroy(other.gameObject);
+            //Decrament the pickup count 
+            pickupCount -= 1;
+            // run chack pickups function
+            CheckPickups();
+        }
+    }
+
+    void CheckPickups()
+    {
+        print("PickUps left: " + pickupCount);
+
+        if (pickupCount == 0)
+        {
+            print("Yay! You killed them all!");
+        }
     }
 }
